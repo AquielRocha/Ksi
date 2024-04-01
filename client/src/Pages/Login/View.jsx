3322314f4ad3027d162c81327770e23000866db1
Aquiel Rocha <Aquiel123@gmail.com>
2024-03-31 21:20:19 -0300

    commit

--- client/src/Pages/Login/View.jsx
+++ client/src/Pages/Login/View.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
-import Background from "../../Components/Background/Background";
-import Header from "../../Components/Header/Headers";
+import logo from "../../Components/Assets/logo-princ.jpg";
+import Background2 from "../../Components/Background-Inicial/background2";
+
 const LoginView = ({
   userName,
   password,
@@ -14,49 +15,48 @@ const LoginView = ({
 }) => {
   return (
     <div className="body">
-      <Link to={"/"}>
-        <button>Página Principal</button>
-      </Link>
-
       {error && <div className="alert">{error}</div>}
+<div className="mover">
+      <div className="login-form">
+        <div className="cont2">
+          <img className="seg-img" src={logo} alt="Logo" />
 
-      <div className="conteiner">
-        <Header />
-
-        <div className="login-form">
-          <h1 className="hlo">Log in</h1>
-          <form onSubmit={handleLogin}>
-            <div className="input-box">
-              <label htmlFor="">Insira seu Usuário</label>
-              <FaUser className="icon" />
-              <input
-                type="text"
-                placeholder="Usuário"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-              />
-            </div>
-            <div className="input-box">
-              <label htmlFor="">Senha</label>
-              <RiLockPasswordLine className="icon" />
-              <input
-                type="password"
-                placeholder="Senha"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <Link to="/">
-              <span className="spaa">Esqueceu a senha?</span>
-            </Link>
-            <br />
-            <button type="submit" className="btn-go">
-              Login
-            </button>
-          </form>
+          <h1 className="hlo">LOG IN</h1>
         </div>
+
+        <form onSubmit={handleLogin}>
+          <div className="input-box">
+            <FaUser className="icon" />
+            <input
+              type="text"
+              placeholder="INSIRA SEU USUÁRIO"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="input-box">
+            <RiLockPasswordLine className="icon" />
+            <input
+              type="password"
+              placeholder="INSIRA A SENHA "
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="vtar">
+            <button type="submit" className="btn-go">
+              LOGAR
+            </button>
+            <Link to="/register" style={{ textDecoration: "none" }}>
+              <span className="spaa">NÃO TENHO CONTA</span>
+            </Link>
+          </div>
+        </form>
+        </div>
+
       </div>
-      <Background />
+      <Background2 />
     </div>
   );
 };
